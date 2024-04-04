bf6cd7bab64e8c26e795ef1eddee4f55c7586634
InfiniteNet <117833353+InfiniteNet@users.noreply.github.com>
2024-04-04 09:36:48 -0300

    Update senharoot.sh

--- senharoot.sh
+++ senharoot.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+
+# reset shell colors
+tput init
+
+# https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  PROJECT_ROOT="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$PROJECT_ROOT/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+PROJECT_ROOT="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+# required imports
+source "${PROJECT_ROOT}"/utils/manifest.sh
+
 clear
 [[ $(grep -c "prohibit-password" /etc/ssh/sshd_config) != '0' ]] && {
 	sed -i "s/prohibit-password/yes/g" /etc/ssh/sshd_config
@@ -20,21 +36,6 @@ clear
 } > /dev/null
 service ssh restart > /dev/null
 
-# reset shell colors
-tput init
-
-# https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  PROJECT_ROOT="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$PROJECT_ROOT/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-PROJECT_ROOT="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
-
-# required imports
-source "${PROJECT_ROOT}"/utils/manifest.sh
-
 clear; echo -e "\033[1;32m PRIMEIRO DE O COMANDO sudo -i\033[0m"; sleep 2s; passwd
 
 echo "America/Sao_Paulo" > /etc/timezone
@@ -45,11 +46,12 @@ echo -e "\E[44;1;37m           Acesso a rede           \E[0m"
 echo ""
 echo -e "                \033[1;31mATENCAO"
 echo ""
-echo -e "\033[1;32mSERVIÇO ESPECIFICO PARA MAQUINAS ARM"
+echo -e "\033[1;32mSERVIÇO PARA MAQUINAS ARM"
 echo ""
 echo -e "\033[1;36mINICIANDO LIBERAÇÃO"
 echo ""
 echo -e "\033[1;33mAGUARDE..."
+echo -e "\033[1;33mPRONTO PARA USO!"
 apt-get update -y > /dev/null 2>&1
 apt-get uograde -y > /dev/null 2>&1
 iptables -F && iptables -t nat -F > /dev/null 2>&1
